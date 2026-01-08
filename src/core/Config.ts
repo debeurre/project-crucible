afd8f49e24b1a1c3399396188deb66c63e08baa0
deBeurre <228152502+debeurre@users.noreply.github.com>
2026-01-07 21:23:41 -0500

    chore: reorganize Config.ts and manual parameter tweaks

--- src/core/Config.ts
+++ src/core/Config.ts
@@ -1,20 +1,25 @@
 export const CONFIG = {
+    // world
     TILE_SIZE: 32,
-    WORLD_WIDTH: 60, // tiles
-    WORLD_HEIGHT: 35, // tiles
-    MAX_SPRIGS: 2000,
-    MAX_SPEED: 50.0, // Pixels per second
-    SPRIG_RADIUS: 5.0,
-    SCENT_STRENGTH: 500.0,
-    GRID_SIZE: 16,
-    RAIL_MAGNET_STRENGTH: 5.0,
-    WANDER_COOLDOWN: 5.0,
+    WORLD_WIDTH: 60,    // tiles
+    WORLD_HEIGHT: 35,   // tiles
+    GRID_SIZE: 16,      // a*
     INTERACTION_BUFFER: 5,
+    // sprigs
+    MAX_SPRIGS: 2000,
+    MAX_SPEED: 50.0,    // Pixels per second
+    SPRIG_RADIUS: 5.0,
+    // tools
+    SCENT_STRENGTH: 500.0,
     SCENT_WEIGHT: 0.1,
+    // wandering
+    WANDER_COOLDOWN: 5.0,
+    RAIL_MAGNET_STRENGTH: 5.0,
     RAIL_WEIGHT: 0.8,
-    HAULER_DRAG: 0.8,
-    HAULER_STEEP: 3.0,
-    HAULER_STEP_DURATION: 600,
+    // hauling
+    HAULER_DRAG: 1,
+    HAULER_STEEP: 4,
+    HAULER_STEP_DURATION: 1000
 };
 
 export const SCREEN_WIDTH = CONFIG.TILE_SIZE * CONFIG.WORLD_WIDTH;
